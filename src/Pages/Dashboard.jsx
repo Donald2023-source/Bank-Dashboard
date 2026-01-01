ae7b867337026918ca75ad013e5eb6ec6059622c
Donald2023-source <donalddyusuf@gmail.com>
2026-01-01 17:54:26 +0100

    feat: responsive sidenav

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -6,14 +6,14 @@ export default function Dashboard() {
   return (
     <>
       <div className="w-full">
-        <section className="flex md:flex-row w-full  flex-col items-center gap-10">
-          <div className=" md:w-2/3 scroll overflow-x-auto">
+        <section className="flex lg:flex-row w-full  flex-col items-center gap-10">
+          <div className="w-full lg:w-2/3 scroll overflow-x-auto">
             <span className="flex items-center justify-between py-4">
               <h4 className="text-lg font-semibold text-[#343C6A]">My Cards</h4>
               <Link className="font-semibold text-[#343C6A]">See All</Link>
             </span>
             <div className="flex w-full justify-between gap-8 items-center overflow-x-auto">
-              <div className=" bg-linear-to-r from-[#4C49ED] to-[#0A06F4] w-full text-white rounded-2xl md:w-[47%] shrink-0">
+              <div className=" bg-linear-to-r from-[#4C49ED] to-[#0A06F4] w-full text-white rounded-2xl lg:w-[47%] shrink-0">
                 <DashboardCard chipColor={"white"} />
               </div>
               <div className="w-full rounded-2xl border-2 border-gray-300 bg-white shadow md:w-[47%] shrink-0">
